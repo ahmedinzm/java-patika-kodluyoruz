c207202c5f3f3b907bee185c92855ee2f2885092
Ferhat karakaş <50783104+ahmedinzm@users.noreply.github.com>
2023-05-10 15:30:59 +0300

    Array Harmonic

---
+++ ArrayHarmonic.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; // Dizi
+        double sum = 0.0; // Toplam değeri tutmak için değişken tanımlama
+
+        // Elemanların harmonik serisini hesapla
+        for (int i = 0; i < numbers.length; i++) {
+            sum += (double)1/numbers[i];
+        }
+
+        // Harmonik ortalama hesabı
+        double harmonicMean = numbers.length / sum;
+
+        System.out.println("Dizinin harmonik ortalaması: " + harmonicMean);
+    }
+}
